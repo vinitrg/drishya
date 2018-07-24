752be89d1d8479831cf6d804d76b481edc915586
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2018-07-24 16:55:15 +0530

    adding localhost as allowed host

--- drishya/drishya/settings.py
+++ drishya/drishya/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-=&^i+)p9*5)cuwyc1saw_t@#wxn5!#$98_66(z(im6l$0=3j8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
